52333d027aeafd4075e62ea447c7a77f0f00f093
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-20 17:47:54 +0530

    start new Project and complete Setup also

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from "cors"
+import "dotenv/config"
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.json({
+        message: "CodeArena API is running..."
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
