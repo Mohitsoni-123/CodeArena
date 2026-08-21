6130c5fa2b3a41d4eeba69954616ef0480c4ebb9
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-21 16:05:28 +0530

    Login Authentication Done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
 
 const register = async(req, res)=>{
     try{
@@ -42,5 +43,56 @@ const register = async(req, res)=>{
         })
     }
 }
+
+export const login = async(req, res)=>{
+    try{
+
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({
+            message: "Email and Password are required"  
+            });
+        }
+        //find user
+        const user = await User.findOne({ email })
+        if(!user){
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+        const isPasswordCorrect = bcrypt.compare(password, user.hashedPassword);
+
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                message: "Invalid email or password"
+            })
+        }
+        //generate JWT
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                role: user.role
+            },
+            process.env.JWT_SECRET,{expiresIn: "7d"}
+        );
+        res.status(200).json({
+            message: "Login Successful",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        });
+    }catch(error){
+        console.error("Login Error: ",error.message);
+
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+}
+
 
 export default register;
