361e4137906af952a539525cbd511a5edc622c76
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-22 16:17:48 +0530

    complete getProblems controller logic

---
+++ server/controllers/problemController.js
@@ -0,0 +1,44 @@
+import Problem from "../models/Problem";
+
+export const createProblem = async(req, res)=>{
+    try{
+        const {title, description, difficulty, topics, examples, constraints, starterCode, testCases} =req.body;
+
+        if(!title || !description || !difficulty){
+            return res.status(400).json({
+                message: "Title, description and difficulty are required"
+            });
+        }
+        const problem = await Problem.create({
+            title, description, difficulty, topics, examples, constraints, starterCode, testCases, createdBy: req.user.userId
+        });
+        res.status(201).json({
+            message: "Problem created successfully",
+            problem
+        })
+
+    }catch(error){
+        console.error("Create Problem Error:", error.message);
+        res.status(500).json({
+            message: "Server error"
+        });
+    }
+}
+
+
+export const getProblems = async (req, res)=>{
+    try{
+        const problems = (await Problem.find().select("-testCases")).toSorted({ createdAt: -1 });
+        res.status(200).json({
+            count: problems.length,
+            problems
+        })
+    }catch(error){
+        console.error("Get Problem Error:", error.message);
+        res.status(500).json({
+            message: "Server error"
+        })
+    }
+}
+
+
