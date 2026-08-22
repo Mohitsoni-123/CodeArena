5635da366a6f7e27135859acd8254b048f5e4150
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-22 16:02:10 +0530

    complete models->Problem.js

---
+++ server/models/Problem.js
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+
+const problemSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        difficulty: {
+            type: String,
+            enum: ["Easy", "Medium", "Hard"],
+            required: true
+        },
+        topics: [
+            {
+                type: String
+            }
+        ],
+        example: [
+            {
+                input: {
+                    type: String,
+                    required: true
+                },
+                output: {
+                    type: String,
+                    required: true
+                },
+                explanation: {
+                    type: String
+                }
+            }
+        ],
+        constraints: [
+            {
+                trpe: String
+            }
+        ],
+        starterCode: {
+            typr: String,
+            default: ""
+        },
+        testCases: [
+            {
+                input: {
+                    type: String,
+                    required: true
+                },
+                expectedOutput: {
+                    type: String,
+                    required: true
+                },
+                isHidden: {
+                    type: Boolean,
+                    default: false
+                }
+            }
+        ],
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Problem = mongoose.model("Problem", problemSchema);
+
+export default Problem;
