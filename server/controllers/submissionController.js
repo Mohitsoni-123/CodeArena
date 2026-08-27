95bd1f586aec3af765e4bb3fa101db64683538c7
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-27 11:48:37 +0530

    connect jdoodleService to submissionController

--- server/controllers/submissionController.js
+++ server/controllers/submissionController.js
@@ -1,44 +1,63 @@
 import Submission from "../models/Submission.js";
 import Problem from "../models/Problem.js";
+import { executeCode } from "../services/jdoodleService.js";
 
-export const createSubmission = async (req, res)=>{
+export const createSubmission = async (req, res) => {
     try {
-        const { problemId, language, code } = req.body;
+        const { problemId, language, code, stdin = "" } = req.body;
 
-        //check required fields
-        if(!problemId || !language || !code){
+        // Check required fields
+        if (!problemId || !language || !code) {
             return res.status(400).json({
                 message: "Problem ID, language and code are required"
             });
         }
 
-        //check Problem
+        // Check problem
         const problem = await Problem.findById(problemId);
-        if(!problem){
+
+        if (!problem) {
             return res.status(404).json({
                 message: "Problem not found"
             });
         }
 
-        //create submission
+        // Execute code using JDoodle
+        const result = await executeCode({
+            language,
+            code,
+            stdin
+        });
+
+        // Decide status
+        const status =
+            result.statusCode === 200 ? "Accepted" : "Error";
+
+        // Create submission
         const submission = await Submission.create({
             user: req.user.userId,
             problem: problemId,
             language,
             code,
-            status: "Pending",
+            status,
             totalTestCases: problem.testCases.length
         });
+
         res.status(201).json({
-            message: "Submission create successfully",
-            submission
+            message: "Code executed successfully",
+            submission,
+            result
         });
+
     } catch (error) {
         console.error(
-            "Create Submission Error:", error.message
+            "Create Submission Error:",
+            error.response?.data || error.message
         );
+
         res.status(500).json({
-            message: "Server error"
+            message: "Code execution failed",
+            error: error.response?.data || error.message
         });
     }
-}
+};
