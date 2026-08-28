82936d70ccb274bab3299f78415d1abb36fd6973
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-08-28 22:03:05 +0530

    create Home.jsx file and write some code

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to CodeArena</h1>
+
+      <p>Practice coding problems and improve your problem-solving skills</p>
+
+      <Link to="/problems">
+        Explore Problems
+      </Link>
+    </div>
+  )
+}
+
+export default Home
