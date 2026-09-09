aca7eb2521dca44f6480c1d6664bccbaa76c08db
Mohit Soni <mohitsonikadel2006@gamil.com>
2026-09-09 12:53:25 +0530

    deploy on vercel

---
+++ server/server.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import cors from "cors"
+import "dotenv/config"
+
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import authMiddleware from './middleware/authMiddleware.js';
+import adminMiddleware from './middleware/adminMiddleware.js';
+import problemRoutes from "./routes/problemRoutes.js";
+import submissionRoutes from "./routes/submissionRoutes.js";
+
+import adminRoutes from "./routes/adminRoutes.js";
+
+
+
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://code-arena-eta-eight.vercel.app",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
+app.use(express.json());
+connectDB();
+
+
+
+
+
+
+app.use("/api/auth",authRoutes);
+app.use("/api/problems", problemRoutes);
+app.use("/api/submissions", submissionRoutes)
+app.use("/api/admin", adminRoutes);
+
+app.get(
+    "/api/admin/test",
+    authMiddleware,
+    adminMiddleware,
+    (req, res) => {
+        res.json({
+            message: "Welcome Admin 👑"
+        });
+    }
+);
+
+
+app.get("/", (req, res)=>{
+    res.json({
+        message: "CodeArena API is running..."
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+})
